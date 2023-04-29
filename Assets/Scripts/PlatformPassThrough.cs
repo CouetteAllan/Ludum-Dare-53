385af3ba55bd364c74b49d41334ec5aa27455e8e
COUETTE Allan <55735176+CouetteAllan@users.noreply.github.com>
2023-04-29 23:44:09 +0200

    Drop Down fully done

--- Assets/Scripts/PlatformPassThrough.cs
+++ Assets/Scripts/PlatformPassThrough.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class PlatformPassThrough : MonoBehaviour
 {
     private bool canPass = false;
+    private Collider2D playerCollider;
+    private Collider2D platformCollider;
 
     void Start()
     {
-        
+        platformCollider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
@@ -23,19 +25,28 @@ public class PlatformPassThrough : MonoBehaviour
         if(collision.gameObject.TryGetComponent<PlayerController>(out PlayerController player))
         {
             player.OnDropDown += Player_OnDropDown;
+            playerCollider = collision.collider;
         }
     }
 
     private void Player_OnDropDown()
     {
-        throw new System.NotImplementedException();
+        StartCoroutine(CollisionCoroutine());
+    }
+
+    private IEnumerator CollisionCoroutine()
+    {
+        float timerNoCollision = 0.5f;
+        Physics2D.IgnoreCollision(platformCollider, playerCollider, true);
+        yield return new WaitForSeconds(timerNoCollision);
+        Physics2D.IgnoreCollision(platformCollider, playerCollider, false);
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.TryGetComponent<PlayerController>(out PlayerController player))
         {
-
+            player.OnDropDown -= Player_OnDropDown;
         }
     }
 }
